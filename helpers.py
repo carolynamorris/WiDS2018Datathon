7e8cfd7fe0d316430851e37cd0204d900f3a9e3b
Carolyn Morris <cmorris1103@gmail.com>
2018-02-15 18:01:42 -0500

    Start comparing the predictions from the two classifiers

--- helpers.py
+++ helpers.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-def format_submission(predictions, size):
+def format_submission(predictions):
 	  predictions = np.round([p[1] for p in predictions], 1)
-	  test_id = range(0, size)
+	  test_id = range(0, len(predictions))
 	  d = {'test_id': test_id, 'is_female': predictions}
 	  df = pd.DataFrame(d)
 	  df = df[['test_id', 'is_female']]
